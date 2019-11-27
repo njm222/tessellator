5eb8a7aef426d55a3bc0b4d9cca337ea207d3cdf
Nathan <nathan.john.menezes@gmail.com>
2019-11-27 15:16:36 -0800

    final package changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var sm = require('sitemap');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');

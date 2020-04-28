0908e3fa1fb0e6cf8be8df9ca3a16c534ba1e74b
njm222 <nathan.john.menezes@gmail.com>
2020-04-28 16:09:52 +0530

    added webpack compression for vue

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new CompressionPlugin()
+        ]
+    }
+}

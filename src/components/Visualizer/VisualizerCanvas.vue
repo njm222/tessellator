51f296ad38820050dc5e8cff2ea1716db22b3ab4
njm222 <nathan.john.menezes@gmail.com>
2020-05-23 18:15:58 +0530

    increased distance of shapeGrid

--- src/components/Visualizer/VisualizerCanvas.vue
+++ src/components/Visualizer/VisualizerCanvas.vue
@@ -506,7 +506,7 @@ export default class VisualizerCanvas extends Vue {
     let x = 0
     let y = 0
     let z = 0
-    const distance = 25
+    const distance = 35
     let shapeCount = 0
     const lim = 12
 

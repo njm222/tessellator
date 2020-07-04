dc171dc371b2397b11bb14b7cac1652fff930b2d
NM <nathan.john.menezes@gmail.com>
2020-07-04 11:03:18 +0530

    added play button for last played item

--- src/components/ActiveUsers/LastPlayedItem.vue
+++ src/components/ActiveUsers/LastPlayedItem.vue
@@ -1,5 +1,6 @@
 <template>
   <div v-if='trackDetails' class="item">
+    <i @click="playTrack(deviceID, trackDetails.uri)" class="icon play"></i>
     <a v-bind:href='trackDetails.external_urls.spotify' target='_blank'>
       {{ trackDetails.name }}
     </a>
@@ -18,12 +19,32 @@ export default class LastPlayedItem extends Vue {
   @Prop({ required: true })
   trackDetails!: SpotifyApi.TrackLinkObject
 
-  constructor () {
-    super()
-    console.log(this.trackDetails)
+  get deviceID () {
+    return this.$store.state.deviceID
+  }
+
+  private playTrack (device_id: string, track: string): void {
+    console.log(track)
+    console.log(device_id)
+    console.log(this.$store.state.accessToken)
+    Vue.axios.put(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
+      uris: [track]
+    }, {
+      headers: { Authorization: 'Bearer ' + this.$store.state.accessToken }
+    }).then(res => {
+      console.log(res)
+    }).catch((error) => {
+      console.log(error)
+    })
   }
 }
 </script>
 
 <style scoped>
+  .item .icon.play {
+    display: inline-flex;
+    margin: 0 0.5em -6px 0;
+    width: 20px;
+    height: 20px;
+  }
 </style>

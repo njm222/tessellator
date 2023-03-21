28ee95a245b848356bb3841f38dbd85ffbc6b6c9
njm222 <nathan.john.menezes@gmail.com>
2023-03-21 09:48:45 +0530

    cleanup portal

--- apps/tessellator/src/components/canvas/Portal.tsx
+++ apps/tessellator/src/components/canvas/Portal.tsx
@@ -4,68 +4,74 @@ import { useFrame } from "@react-three/fiber";
 import { PerspectiveCamera, RenderTexture } from "@react-three/drei";
 import { EffectComposer, SelectiveBloom } from "@react-three/postprocessing";
 import { usePortal } from "../../utils/portalContext";
+import { Camera } from "@react-three/fiber/dist/declarations/src/core/events";
+
+const [portalWidth, portalHeight] = [3, 7];
 
 function Portal({ children }: { children: ReactNode }) {
   const meshRef = useRef(new Mesh());
   const portalCamRef = useRef(false);
   const { inPortal, setInPortal } = usePortal();
 
-  // simplify
+  function handleOutsidePortal(camera: Camera, delta: number) {
+    if (camera.position.distanceTo(meshRef.current.position) > 5) {
+      // reset camera if moved / rotated
+      camera.position.lerp(new Vector3(0, 0, 10), delta);
+    }
+    // if camera is close enough lerp closer
+    if (
+      !portalCamRef.current &&
+      camera.position.distanceTo(meshRef.current.position) < 5
+    ) {
+      // position camera center
+      camera.position.lerp(new Vector3(0, 0, 2), delta * 5);
+      camera.updateProjectionMatrix();
+      camera.updateMatrixWorld();
+    }
+    // if camera is very close switch cams
+    if (
+      !portalCamRef.current &&
+      camera.position.distanceTo(meshRef.current.position) <= 2.1
+    ) {
+      // rotate camera center
+      camera.quaternion.slerp(new Quaternion(-Math.PI * 2, 0, 0, 1), delta * 5);
+      setInPortal(true);
+      portalCamRef.current = true;
+    }
+  }
+
+  function handleInsidePortal(camera: Camera, delta: number) {
+    // reset camera if moved / rotated
+    camera.position.lerp(new Vector3(0, 0, 3), delta * 2);
+    // if cam is far lerp further
+    if (camera.position.z > 5) {
+      // rotate camera center
+      camera.quaternion.slerp(new Quaternion(-Math.PI * 2, 0, 0, 1), delta * 5);
+      // position camera center
+      camera.position.lerp(new Vector3(0, 0, 10), delta * 5);
+      camera.updateProjectionMatrix();
+      camera.updateMatrixWorld();
+    }
+    // if cam is very far switch cams
+    if (camera.position.z > 8) {
+      setInPortal(false);
+      portalCamRef.current = false;
+    }
+  }
+
   useFrame((state, delta) => {
-    // !Visualizer state
-    if (!inPortal && state.camera && meshRef.current?.position) {
-      if (state.camera.position.distanceTo(meshRef.current.position) > 5) {
-        // reset camera if moved / rotated
-        state.camera.position.lerp(new Vector3(0, 0, 10), delta);
-      }
-      // if camera is close enough lerp closer
-      if (
-        !portalCamRef.current &&
-        state.camera.position.distanceTo(meshRef.current.position) < 5
-      ) {
-        // position camera center
-        state.camera.position.lerp(new Vector3(0, 0, 2), delta * 10);
-        state.camera.updateProjectionMatrix();
-        state.camera.updateMatrixWorld();
-      }
-      // if camera is very close switch cams
-      if (
-        !portalCamRef.current &&
-        state.camera.position.distanceTo(meshRef.current.position) <= 2.1
-      ) {
-        // rotate camera center
-        state.camera.quaternion.slerp(
-          new Quaternion(-Math.PI * 2, 0, 0, 1),
-          delta * 10
-        );
-        setInPortal(true);
-        portalCamRef.current = true;
-        return;
-      }
+    if (!state.camera) {
+      return;
+    }
+
+    if (!inPortal && meshRef.current?.position) {
+      handleOutsidePortal(state.camera, delta);
       return;
     }
 
     // Visualizer state
     if (portalCamRef.current) {
-      // reset camera if moved / rotated
-      state.camera.position.lerp(new Vector3(0, 0, 3), delta * 2);
-      // if cam is far lerp further
-      if (state.camera.position.z > 5) {
-        // rotate camera center
-        state.camera.quaternion.slerp(
-          new Quaternion(-Math.PI * 2, 0, 0, 1),
-          delta * 5
-        );
-        // position camera center
-        state.camera.position.lerp(new Vector3(0, 0, 10), delta * 10);
-        state.camera.updateProjectionMatrix();
-        state.camera.updateMatrixWorld();
-      }
-      // if cam is very far switch cams
-      if (state.camera.position.z > 8) {
-        setInPortal(false);
-        portalCamRef.current = false;
-      }
+      handleInsidePortal(state.camera, delta);
     }
   });
 
@@ -78,8 +84,6 @@ function Portal({ children }: { children: ReactNode }) {
 
 export default memo(Portal);
 
-const [portalWidth, portalHeight] = [3, 7];
-
 const PortalScene = ({
   children,
   meshRef,
@@ -87,19 +91,19 @@ const PortalScene = ({
   children: ReactNode;
   meshRef: MutableRefObject<Mesh>;
 }) => {
-  const meshTrimRef = useRef(new Mesh());
+  const meshOutlineRef = useRef(new Mesh());
   return (
     <group position={[0, 1, 0]}>
       <EffectComposer>
         <SelectiveBloom
-          selection={meshTrimRef}
+          selection={meshOutlineRef}
           luminanceThreshold={0.1}
           luminanceSmoothing={0.1}
           width={512}
           height={512}
         />
       </EffectComposer>
-      <mesh ref={meshTrimRef} position={[0, 0, -0.02]}>
+      <mesh ref={meshOutlineRef} position={[0, 0, -0.02]}>
         <planeGeometry args={[portalWidth + 0.1, portalHeight + 0.1]} />
         <meshBasicMaterial color="red" />
       </mesh>

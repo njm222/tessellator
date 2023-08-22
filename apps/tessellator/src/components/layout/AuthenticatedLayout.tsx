c1d9692b05ee2dcedb56d544fd96f5276960d9fb
njm222 <nathan.john.menezes@gmail.com>
2023-08-22 12:01:13 +0530

    upgrade ts

--- apps/tessellator/src/components/layout/AuthenticatedLayout.tsx
+++ apps/tessellator/src/components/layout/AuthenticatedLayout.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import React, { ReactNode } from "react";
+import dynamic from "next/dynamic";
 
-import { AnalyserProvider } from "../../utils/analyserContext";
 import { PlayerProvider } from "../../utils/playerContext";
 import { PortalProvider } from "../../utils/portalContext";
-import { ControlsProvider } from "../dom/controls/controlsContext";
 import { MouseActivityProvider } from "../dom/controls/mouseActivityContext";
+
+const AnalyserProvider = dynamic(
+  () =>
+    import("../../utils/analyserContext").then((mod) => mod.AnalyserProvider),
+  { ssr: false }
+);
+const ControlsProvider = dynamic(
+  () =>
+    import("../dom/controls/controlsContext").then(
+      (mod) => mod.ControlsProvider
+    ),
+  { ssr: false }
+);
 
 export const AuthenticatedLayout = ({ children }: { children: ReactNode }) => {
   return (

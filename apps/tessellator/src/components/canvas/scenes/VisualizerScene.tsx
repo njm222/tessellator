809fa12ced2a44cdabab1737a1345db7351b7771
njm222 <nathan.john.menezes@gmail.com>
2023-08-31 12:13:28 +0530

    optimize bridge model

--- apps/tessellator/src/components/canvas/scenes/VisualizerScene.tsx
+++ apps/tessellator/src/components/canvas/scenes/VisualizerScene.tsx
@@ -14,7 +14,11 @@ const OuterScene = () => {
   return inPortal ? null : (
     <>
       <Stars count={5000} depth={50} factor={2} fade radius={10} />
-      <Bridge position={[0, -2.5, 5]} rotation={[0, Math.PI / 2, 0]} />
+      <Bridge
+        scale={2}
+        position={[0, -2.5, 5]}
+        rotation={[0, Math.PI / 2, 0]}
+      />
     </>
   );
 };

15158b276c4e2b9c16d095766a37bf8abdf08a2e
njm222 <nathan.john.menezes@gmail.com>
2023-09-05 11:56:13 +0530

    feat: enable pwa and optimize seo

--- apps/tessellator/src/app/layout.tsx
+++ apps/tessellator/src/app/layout.tsx
@@ -14,6 +14,7 @@ export const metadata = {
 const tomorrow = Tomorrow({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
+  display: "swap",
 });
 
 export default function RootLayout({ children }: { children: ReactNode }) {

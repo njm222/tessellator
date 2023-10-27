91985d9d1591e6cdf666842d4fb1a851355b2448
njm222 <nathan.john.menezes@gmail.com>
2023-10-27 12:57:21 +0400

    rebase main

--- apps/tessellator/src/components/canvas/scenes/VisualizerScene.tsx
+++ apps/tessellator/src/components/canvas/scenes/VisualizerScene.tsx
@@ -2,7 +2,6 @@ import { useEffect, useRef } from "react";
 import { BakeShadows, Stars } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { Bloom, EffectComposer, Pixelation } from "@react-three/postprocessing";
-import { PixelationEffect } from "postprocessing";
 
 import Bridge from "../../models/Bridge";
 import Portal from "../Portal";

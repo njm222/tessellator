693f7d4903709bed81271f1c66e7eeec14e89859
njm222 <nathan.john.menezes@gmail.com>
2023-11-09 11:00:47 +0530

    prefix spotify-analyser with @tessellator

--- apps/tessellator/src/utils/playerContext.tsx
+++ apps/tessellator/src/utils/playerContext.tsx
@@ -7,9 +7,9 @@ import {
   useState,
 } from "react";
 import { captureException } from "@sentry/nextjs";
+import SpotifyAnalyser from "@tessellator/spotify-analyser";
 import { useToast } from "@tessellator/ui";
 import { StartPlaybackOptions } from "core";
-import SpotifyAnalyser from "spotify-analyser";
 
 import { useAuth } from "./authContext";
 import {

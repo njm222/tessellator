68d7e1ae25b47bde66015ceab3eea3fc5151d6a8
njm222 <nathan.john.menezes@gmail.com>
2023-12-21 10:52:36 +0530

    update sitemap & robots

--- apps/tessellator/src/app/sitemap.ts
+++ apps/tessellator/src/app/sitemap.ts
@@ -17,6 +17,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${environment.frontendUrl}/live`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
       url: `${environment.frontendUrl}/about`,
       lastModified: new Date(),
       changeFrequency: "monthly",

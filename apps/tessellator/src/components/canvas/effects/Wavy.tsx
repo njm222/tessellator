0da96d4d817bfdf1bebc78d2e11f324637619148
njm222 <nathan.john.menezes@gmail.com>
2024-01-08 10:13:58 +0530

    finishing touches

--- apps/tessellator/src/components/canvas/effects/Wavy.tsx
+++ apps/tessellator/src/components/canvas/effects/Wavy.tsx
@@ -1,18 +1,16 @@
 import { forwardRef, useMemo } from "react";
 import { Effect } from "postprocessing";
-import { MathUtils, Uniform, WebGLRenderer,WebGLRenderTarget } from "three";
+import { MathUtils, Uniform, WebGLRenderer, WebGLRenderTarget } from "three";
 
 // @ts-ignore
 import fragment from "./glsl/shader.frag";
 
 let _uDistortion: number, _uDistortion2: number, _uSpeed: number;
 
-// Effect implementation
 export class WavyEffect extends Effect {
   constructor({ distortion = 3.0, distortion2 = 6.0, speed = 0.5 }) {
     super("WavyEffect", fragment, {
-      uniforms: new Map<string, Uniform>([
-        ["texture", new Uniform(null)],
+      uniforms: new Map<string, Uniform<number>>([
         ["distortion", new Uniform(distortion)],
         ["distortion2", new Uniform(distortion2)],
         ["speed", new Uniform(speed)],
@@ -31,31 +29,22 @@ export class WavyEffect extends Effect {
    * @param {WebGLRenderTarget} inputBuffer - A frame buffer that contains the result of the previous pass.
    * @param {Number} [deltaTime] - The time between the last frame and the current one in seconds.
    */
-
-  // getUniform(u: string) {
-  //   const uniform = this.uniforms.get(u);
-
-  //   if (!uniform) return;
-
-  //   return uniform.value;
-  // }
-
   update(
     renderer: WebGLRenderer | null,
     inputBuffer: WebGLRenderTarget | null,
     deltaTime: number
   ) {
-    this.uniforms.get("distortion").value = MathUtils.lerp(
+    this.uniforms.get("distortion")!.value = MathUtils.lerp(
       this.uniforms.get("distortion")?.value || 0,
       _uDistortion,
       deltaTime
     );
-    this.uniforms.get("distortion2").value = MathUtils.lerp(
+    this.uniforms.get("distortion2")!.value = MathUtils.lerp(
       this.uniforms.get("distortion2")?.value || 0,
       _uDistortion2,
       deltaTime
     );
-    this.uniforms.get("speed").value = MathUtils.lerp(
+    this.uniforms.get("speed")!.value = MathUtils.lerp(
       this.uniforms.get("speed")?.value || 0,
       _uSpeed,
       deltaTime
@@ -70,7 +59,14 @@ export class WavyEffect extends Effect {
 }
 
 export const Wavy = forwardRef(
-  ({ distortion = 3.0, distortion2 = 5.0, speed = 0.2 }: any, ref) => {
+  (
+    {
+      distortion = 3.0,
+      distortion2 = 5.0,
+      speed = 0.2,
+    }: { distortion: number; distortion2: number; speed: number },
+    ref
+  ) => {
     const effect = useMemo(
       () =>
         new WavyEffect({
@@ -83,3 +79,5 @@ export const Wavy = forwardRef(
     return <primitive dispose={null} object={effect} ref={ref} />;
   }
 );
+
+Wavy.displayName = "Wavy";
